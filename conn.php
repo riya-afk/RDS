a5ca3dd3581d38dc74a8148651185e91ceb57539
riya-afk <75583237+riya-afk@users.noreply.github.com>
2020-12-12 20:26:17 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php 
-	$con = mysqli_connect('localhost','root','','studentdb');
+	$con = mysqli_connect('studentdb1.cwya3gxurhic.us-east-1.rds.amazonaws.com','admin','riya12345','studentdb1');
  ?>
